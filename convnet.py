c19eb34edbb1d7756390e13b129f6fea7f5fc58d
monty0157 <kaspermz@hotmail.com>
2017-04-17 13:28:25 +0200

    Added test, improved data import

--- convnet.py
+++ convnet.py
@@ -6,17 +6,25 @@ import parameters as params
 #import image_import as images
 from data_import import unpickle
 
-unpickled_data = unpickle('./data/cifar-10-batches-py/data_batch_2')
+n_classes = 10
+n_images = 50000
+n_images_test = 10000
+training_epochs = 10
+batch_size = 100
 
-dataset = unpickled_data[b'data'].reshape(len(unpickled_data[b'data']),32,32,3)
-labels = unpickled_data[b'labels']
+unpickled_data = unpickle('./data/train_data/')
+unpickled_data_test = unpickle('./data/test_data/')
+
+dataset, labels = unpickled_data
+dataset_test, labels_test = unpickled_data_test
+
+#improving image quality by transposing image instead of reshaping
+dataset = dataset.reshape(n_images,3,32,32).transpose(0,2,3,1)
+dataset_test = dataset_test.reshape(n_images_test,3,32,32).transpose(0,2,3,1)
 
 #convert labels to one-hot vector
 vector_labels = tf.one_hot(labels, depth=10)
-
-n_classes = 10
-training_epochs = 2
-batch_size = 50
+vector_labels_test = tf.one_hot(labels_test, depth=10)
 
 x_input = tf.placeholder(tf.float32, shape=(None,32,32,3), name="x-input")
 y_output = tf.placeholder('float')
@@ -24,8 +32,6 @@ y_output = tf.placeholder('float')
 keep_prob = tf.placeholder('float')
 
 def convolutional_neural_network(data):
-    #data = tf.reshape(data, [-1, 160, 96, 3])
-
     #NAMING CONVENTION: conv3s32n is a convolutional layer with filter size 3x3 and number of filters = 32
     conv3s32n = layers.conv_layer(data, params.weights(depth=3), params.biases())
     conv3s32n = layers.conv_layer(conv3s32n, params.weights(), params.biases())
@@ -41,7 +47,14 @@ def convolutional_neural_network(data):
 
     pool2w2s = layers.pool_layer(conv3s32n)
 
-    conv3s32n = layers.conv_layer(pool2w2s, params.weights(n_filters=64), params.biases(n_filters=64))
+    conv3s32n = layers.conv_layer(pool2w2s, params.weights(n_filters=128), params.biases(n_filters=128))
+    conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=128, n_filters=128), params.biases(n_filters=128))
+    conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=128, n_filters=128), params.biases(n_filters=128))
+    conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=128, n_filters=128), params.biases(n_filters=128))
+    conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=128, n_filters=128), params.biases(n_filters=128))
+    conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=128, n_filters=128), params.biases(n_filters=128))
+
+    '''conv3s32n = layers.conv_layer(pool2w2s, params.weights(n_filters=64), params.biases(n_filters=64))
     conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=64, n_filters=64), params.biases(n_filters=64))
     conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=64, n_filters=64), params.biases(n_filters=64))
     conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=64, n_filters=64), params.biases(n_filters=64))
@@ -52,14 +65,14 @@ def convolutional_neural_network(data):
 
     conv3s32n = layers.conv_layer(pool2w2s, params.weights(depth=64, n_filters=128), params.biases(n_filters=128))
     conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=128, n_filters=128), params.biases(n_filters=128))
-    conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=128, n_filters=128), params.biases(n_filters=128))
+    conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=128, n_filters=128), params.biases(n_filters=128))'''
 
 
-    pool2w2s = layers.pool_layer(conv3s32n)
+    #pool2w2s = layers.pool_layer(conv3s32n)
 
-    conv3s32n = layers.conv_layer(pool2w2s, params.weights(depth=128, n_filters=256), params.biases(n_filters=256))
-    conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=256, n_filters=256), params.biases(n_filters=256))
-    conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=256, n_filters=256), params.biases(n_filters=256))
+    #conv3s32n = layers.conv_layer(pool2w2s, params.weights(depth=128, n_filters=256), params.biases(n_filters=256))
+    #conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=256, n_filters=256), params.biases(n_filters=256))
+    #conv3s32n = layers.conv_layer(conv3s32n, params.weights(depth=256, n_filters=256), params.biases(n_filters=256))
 
     #NAMING CONVENTION: Fully connected layers are just indexed
     fc1 = layers.full_layer(conv3s32n, params.fc_weights(conv3s32n, 1024), params.biases(1024), keep_prob)
@@ -67,17 +80,17 @@ def convolutional_neural_network(data):
     fc3 = layers.full_layer(fc2, params.fc_weights(fc2, 1024), params.biases(1024), keep_prob)
 
 
-    output = layers.output_layer(fc3, params.fc_weights(fc3, n_classes), params.biases(n_classes))
+    output = layers.output_layer(fc1, params.fc_weights(fc1, n_classes), params.biases(n_classes))
 
     return output
 
 def train_neural_network(x):
     prediction = convolutional_neural_network(x)
     with tf.name_scope("cost"):
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y_output))
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y_output))
         optimizer = tf.train.AdamOptimizer().minimize(cost)
         tf.summary.scalar("cost", cost)
-
+    print(prediction, y_output)
     correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y_output, 1))
 
 
@@ -85,63 +98,61 @@ def train_neural_network(x):
        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
        tf.summary.scalar("accuracy", accuracy)
 
-
-
     saver = tf.train.Saver()
     config = tf.ConfigProto()
     config.gpu_options.allow_growth=True
 
     with tf.Session(config=config) as sess:
         summary_op = tf.summary.merge_all()
-        #writer = tf.summary.FileWriter("./logs/saved_model_5", graph=tf.get_default_graph())
-
+        writer = tf.summary.FileWriter("./logs/100_epochs", graph=tf.get_default_graph())
         sess.run(tf.global_variables_initializer())
+        labels = vector_labels.eval().reshape(n_images,10)
 
         #FOR TRAINING THE NETWORK
         for epoch in range(training_epochs):
             epoch_loss = 0
             start = 0
             end = int(batch_size)
-            for j in range(batch_size):
-                epoch_x, epoch_y = dataset[start:end], vector_labels.eval()[start:end]
+            labels = vector_labels.eval().reshape(n_images,10)
+            for j in range(int(n_images/batch_size)):
+                epoch_x, epoch_y = dataset[start:end], labels[start:end]
                 j, c, summary = sess.run([optimizer, cost, summary_op], feed_dict = {x_input: epoch_x, y_output: epoch_y, keep_prob: 1.0 })
                 epoch_loss += c
                 start += int(batch_size)
                 end += int(batch_size)
-                print(epoch_loss)
-                #writer.add_summary(summary, j)
+                writer.add_summary(summary, j)
 
-            print('Epoch', epoch, 'completed out of', 10, 'loss:', epoch_loss, 'Accuracy:', accuracy.eval(feed_dict={x:epoch_x, y_output: epoch_y, keep_prob: 1.0}),)
+            print('Epoch', epoch + 1, 'completed out of', 10, 'loss:', epoch_loss, 'Accuracy:', accuracy.eval(feed_dict={x:epoch_x, y_output: epoch_y, keep_prob: 1.0}),)
 
             #save_path = saver.save(sess, "my-model")
 
         #model_import = tf.train.import_meta_graph('my-model.meta')
         #model_import.restore(sess, tf.train.latest_checkpoint('./'))
 
-        '''print("TEST")
+        print("TEST")
         total_accuracy = 0
         start = 0
         end = int(batch_size)
-        for j in range(int(n_images_test_pos/batch_size)):
-            acc_x, acc_y = image_import_test_pos[start:end], labels_test[start:end]
+        labels_test = vector_labels_test.eval().reshape(n_images_test,10)
+        for j in range(int(n_images_test/batch_size)):
+            acc_x, acc_y = dataset_test[start:end], labels_test[start:end]
             total_accuracy += accuracy.eval(feed_dict={x:acc_x, y_output:acc_y, keep_prob: 1.0})
-            start += int(batch_size+1)
+            start += int(batch_size)
             end += int(batch_size)
 
-        mean_accuracy = total_accuracy/int(n_images_test_pos/batch_size)
-        print(mean_accuracy)'''
+        mean_accuracy = total_accuracy/int(n_images_test/batch_size)
+        print(mean_accuracy)
 
-        '''#FOR TESTING THE NETWORK
-        model_import = tf.train.import_meta_graph('my-model.meta')
-        model_import.restore(sess, tf.train.latest_checkpoint('./'))
+        #FOR TESTING THE NETWORK
+        #model_import = tf.train.import_meta_graph('my-model.meta')
+        #model_import.restore(sess, tf.train.latest_checkpoint('./'))
 
-        result = sess.run(tf.argmax(prediction,1), feed_dict={x: [image_import_train[1]], keep_prob: 1})
+        #result = sess.run(tf.argmax(prediction,1), feed_dict={x: [dataset[1]], keep_prob: 1})
 
-        print (' '.join(map(str, result)))
-
-        plt.imshow(images.image_import_train()[1])
-        plt.show()'''
-
+        #print (' '.join(map(str, result)))
+        print(labels[30000])
+        plt.imshow(dataset[30000])
+        #plt.show()
 
 
 train_neural_network(x_input)
--- data_import.py
+++ data_import.py
@@ -1,9 +1,17 @@
 import pickle
+import os
+import numpy as np
 
 #unpickle CIFAR100 data
 #Each data_dict is a dict with a data (b'data') array containing images
 # and a label array containing labels (b'lables')
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        data_dict = pickle.load(fo, encoding="bytes")
-    return data_dict
+def unpickle(directory):
+    files = os.listdir(directory)
+    dataset = []
+    labels = []
+    for file in files:
+        with open(directory + file, 'rb') as fo:
+            data_dict = pickle.load(fo, encoding="bytes")
+            dataset.append(data_dict[b'data'])
+            labels.append(data_dict[b'labels'])
+    return np.asarray(dataset), np.asarray(labels)
